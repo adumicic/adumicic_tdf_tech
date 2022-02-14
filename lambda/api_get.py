cad5b37b586efcf449a81e9a6c7da0c697751567
Adam Dumicich <acdumicich@swin.edu.au>
2022-02-14 16:42:38 +1100

    First commit

---
+++ lambda/api_get.py
@@ -0,0 +1,118 @@
+from datetime import datetime
+import boto3
+import requests
+from collections.abc import Mapping
+import os
+from dateutil.parser import parse
+import pyarrow as pa
+import pyarrow.parquet as pq
+import time
+import s3fs
+import json
+
+def is_date(string, fuzzy=False):
+    """
+    Return whether the string can be interpreted as a date.
+
+    :param string: str, string to check for date
+    :param fuzzy: bool, ignore unknown tokens in string if True
+    """
+    try: 
+        parse(string, fuzzy=fuzzy)
+        return True
+    except ValueError:
+        return False
+    
+    
+def infer_schema(columns, values):
+    data_types = []
+
+    for key, val in enumerate(values):
+        if val is None:
+            data_types.append((columns[key], pa.null()))
+        elif isinstance(val, int):
+            data_types.append((columns[key], pa.int32()))
+        elif isinstance(val, float):
+            data_types.append((columns[key], pa.float32()))
+        else:
+            data_types.append((columns[key], pa.string()))
+
+    return data_types
+    
+def transform_data(json_data):
+
+    cols = []
+    row_data = []
+
+    for k, v in json_data.items():
+        for kk, vv in v.items():
+            if not isinstance(json_data[k][kk], Mapping):
+                cols.append(kk)
+                row_data.append(vv)
+            else:
+                for sub_key, value in json_data[k][kk].items():
+                    cols.append(sub_key)
+                    row_data.append(value)
+                    
+    return cols, row_data
+
+def call_api():
+    key = '20c80020f4e04898a6342122211706'
+    url = 'http://api.weatherapi.com/v1/current.json?key={}&q=-37.504136, 145.744302&aqi=no'.format(key)
+    response = requests.get(url)
+    return response
+
+def handler(event, context):
+    
+    s3 = s3fs.S3FileSystem()
+
+    test_status = False
+    retries = 3
+    iterations = 0
+
+    s3_bucket = os.getenv('destination_bucket')
+
+    while not test_status and iterations < retries:
+        response = call_api()
+        if response.status_code != 200:
+            print(f'API not responding, status code: {response.status_code}')        
+            time.sleep(3)
+        else:
+            test_status = True
+            print('Response OK. Continuing.')
+        iterations += 1
+
+    if not test_status:
+        print(f'API did not respond. Will retry at next scheduled interval.')
+        
+    try:
+        json_obj = response.json()
+        s3_key = f's3://{s3_bucket}/raw/{datetime.now().year}/{datetime.now().month}/{datetime.now().day}/{datetime.now().hour}.json'
+
+        json_text = json.dumps(json_obj)
+
+        with s3.open(s3_key, 'w') as f:
+            json.dump(json_text, f)
+        print(f'API response saved to s3://{s3_bucket}/raw/')
+
+    except ValueError:
+        print('API response does not contain properly formed JSON. Exiting.')
+
+    columns, values = transform_data(json_obj)
+    parquet_schema = infer_schema(columns, values)
+
+    parquet_data = [pa.array([values[k]]) for k,v in enumerate(columns)]
+
+    batch = pa.RecordBatch.from_arrays(
+        parquet_data,
+        schema = pa.schema(parquet_schema)
+    )
+
+    table = pa.Table.from_batches([batch])
+
+    s3_key = f's3://{s3_bucket}/curated/{datetime.now().year}/{datetime.now().month}/{datetime.now().day}/{datetime.now().hour}/weather.parquet'
+
+    with s3.open(s3_key, 'wb') as f:
+        pq.write_table(table, f)
+        
+    print(f'Parquet file saved to s3://{s3_bucket}/curated/')
--- tdf_test/tdf_test_stack.py
+++ tdf_test/tdf_test_stack.py
@@ -1,8 +1,14 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    RemovalPolicy,
+    aws_lambda as _lambda,
+    aws_iam as iam,
+    aws_events as events,
+    aws_events_targets as targets,
+    aws_s3 as s3,
 )
+import aws_cdk as cdk
+from aws_cdk.aws_events import Rule, Schedule
 from constructs import Construct
 
 class TdfTestStack(Stack):
@@ -10,10 +16,46 @@ class TdfTestStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # s3 bucket      
+        bucket = s3.Bucket(self, 
+        "tdf_bucket",
+         versioned=True,
+         bucket_name='my-tdf-tech-test',
+         removal_policy=RemovalPolicy.DESTROY,
+         auto_delete_objects=True)
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "TdfTestQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # PyArrow Layer
+        pyarrow_layer = _lambda.LayerVersion(self, 'pyarrow_layer',
+                                     code=_lambda.Code.from_asset("layer"),
+                                     description='PyArrow Library',
+                                     compatible_runtimes=[_lambda.Runtime.PYTHON_3_6, _lambda.Runtime.PYTHON_3_7, _lambda.Runtime.PYTHON_3_8],
+                                     removal_policy=RemovalPolicy.DESTROY
+                                     )
+
+        # Lambda policy for S3 access
+        lambda_policy = iam.PolicyStatement(effect=iam.Effect.ALLOW, resources=['*'], actions=['s3:PutObject'])
+
+        # Lambda job
+        my_lambda = _lambda.Function(
+            self, 'TDFHandler',
+            runtime = _lambda.Runtime.PYTHON_3_7,
+            function_name='tdf_handler',
+            code = _lambda.Code.from_asset('lambda'), # folder
+            timeout = cdk.Duration.seconds(300),
+            handler = 'api_get.handler', # file_nyame.handler_function
+            environment={
+                "destination_bucket": bucket.bucket_name
+            },
+            layers=[pyarrow_layer]
+        )
+
+        # Add policy to Lambda
+        my_lambda.add_to_role_policy(lambda_policy)
+
+        # Add Hourly cron job Cloud Watch Event
+        rule = events.Rule(self, "Schedule Rule", schedule=events.Schedule.cron(minute="0") )
+        rule.add_target(targets.LambdaFunction(my_lambda))
+
+        # Optional add-ons
+            # Glue table
+            # Glue crawler
